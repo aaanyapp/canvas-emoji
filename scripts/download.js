298a485cf3c5d24edc6ddac47aa1bd0ee098eb74
chaowei <chaowei.luo@live.cn>
2024-01-19 20:09:57 +0800

    fix: 添加部分多字节的emoji

--- scripts/download.js
+++ scripts/download.js
@@ -5,10 +5,11 @@ const emojis = require("unicode-emoji-json");
 const emojiHandle = require("node-emoji");
 
 let i = 0;
-let faildCount = 0;
+const faildEmoji = [];
 async function dowload(emoji, unicode) {
   i++;
-  unicode = emoji.codePointAt().toString(16);
+  if(emoji.length > 3) unicode = emoji.codePointAt(0).toString(16) + '-'+ emoji.codePointAt(2).toString(16);
+  else unicode = emoji.codePointAt().toString(16);
   let imageUrl = `https://emoji.aranja.com/static/emoji-data/img-apple-160/${unicode}.png`; // 图片的 URL
   let localImageFile = `./emoji_pngs/${emojiHandle.which(emoji)}.png`; // 本地保存图片的路径
   if (fs.existsSync(localImageFile)) {
@@ -20,7 +21,8 @@ async function dowload(emoji, unicode) {
         response.pipe(fs.createWriteStream(localImageFile));
         resolve(`${i}: ${emoji} 下载成功`);
       } else {
-        reject(`${faildCount++}: ${emoji} 下载失败 ${i}`, );
+        faildEmoji.push(emoji);
+        reject(`${faildEmoji.length}: ${emoji} ${unicode} 下载失败 ${i}`, );
       }
     });
   });
@@ -32,7 +34,14 @@ async function main() {
       console.log(err)
     );
   }
-  console.log(`下载完成，失败 ${faildCount} 个`);
+  console.log(`下载完成，失败 ${faildEmoji.length} 个`);
+  fs.writeFile("faildEmoji.json", JSON.stringify(faildEmoji), function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("faildEmoji.json 文件写入成功");
+    }
+  });
 }
 
 main();
